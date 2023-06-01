24de5d38869104cd6fb48b815755f75e14948b6e
Piyush yadav <75525162+piyuxhyadav@users.noreply.github.com>
2023-06-01 11:04:51 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import Graphs from "./components/Graphs";
 
 
-const BASE_URL = `http://data.fixer.io/api/latest?access_key=${YOUR_API_KEY}`
+const BASE_URL = `http://data.fixer.io/api/latest?access_key=f98d61b896c5e6b312ecf79b9f95079d`
 
 const page = () => {
   const isSmallScreen = useMediaQuery("(max-width: 660px)");
